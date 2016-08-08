32d29be87f1969903e70d0b964ab224035df3ea1
Andy Kolean <ajkolean@umich.edu>
2016-08-08 15:42:06 -0400

    start pokemon model class

---
+++ ios-pokemon-pokedex/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  ios-pokemon-pokedex
+//
+//  Created by admin on 8/8/16.
+//  Copyright © 2016 ajkolean. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        _name = name
+        _pokedexId = pokedexId
+    }
+}
